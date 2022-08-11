dce97427d6e4702449b08c57536d65e1f9bb0351
wmiz <will.misback@gmail.com>
2022-08-11 19:13:45 +0700

    Boundary to face

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import particlesOptions from "./particleOptions";
 import Clarifai from "clarifai";
 import FaceRecognition from "./Components/FaceRecognition/FaceRecognition";
 
-console.log(Clarifai);
 const app = new Clarifai.App({
   apiKey: "d10d5980c1f94efcaa606ef5603ca44d",
 });
@@ -32,21 +31,43 @@ class App extends Component {
     super();
     this.state = {
       input: "",
+      imgURL: "",
+      box: {},
     };
   }
 
+  calculateFaceLocation = (data) => {
+    console.log(data);
+    const bounding_box =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+
+    const image = document.getElementById("inputimage");
+
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: bounding_box.left_col * width,
+      topRow: bounding_box.top_row * height,
+      rightCol: width - bounding_box.right_col * width,
+      botRow: height - bounding_box.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = (box) => {
+    this.setState({ box });
+  };
+
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({ input: event.target.value });
   };
 
   onSubmit = () => {
-    console.log("click");
+    this.setState({ imgURL: this.state.input });
     app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL,
-        "https://samples.clarifai.com/metro-north.jpg"
-      )
-      .then((res) => console.log(res))
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((res) => this.calculateFaceLocation(res))
+      .then((box) => this.displayFaceBox(box))
       .catch((err) => console.log(err));
   };
 
@@ -67,7 +88,10 @@ class App extends Component {
             onInputChange={this.onInputChange}
             onSubmit={this.onSubmit}
           ></ImageLinkForm>
-          <FaceRecognition></FaceRecognition>
+          <FaceRecognition
+            box={this.state.box}
+            imgURL={this.state.imgURL}
+          ></FaceRecognition>
         </div>
       </div>
     );
--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -1,26 +1,20 @@
 import React from "react";
 import "./FaceRecognition.css";
 
-const FaceRecognition = ({ onInputChange, onSubmit }) => {
+const FaceRecognition = ({ imgURL, box }) => {
   return (
-    <div>
-      <p className="f3">
-        {"This magic brain will detect faces in your pictures. Give it a try"}
-      </p>
-      <div className="center">
-        <div className="center form pa4 br3 shadow-5">
-          <input
-            onChange={onInputChange}
-            className="f4 pa2 w-70 center bn"
-            type="text"
-          />
-          <button
-            onClick={onSubmit}
-            className="w-30 pa2 grow f4 link ph3 pv dib white bg-light-purple bn"
-          >
-            Detect
-          </button>
-        </div>
+    <div className="center pa4 ma">
+      <div className="absolute mt2">
+        <img id="inputimage" width="500px" height="auto" src={imgURL} alt="" />
+        <div
+          className="bounding-box blur"
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.botRow,
+            left: box.leftCol,
+          }}
+        ></div>
       </div>
     </div>
   );
